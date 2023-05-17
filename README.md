59d95f3e56af4d99048a16e680307d5fb84cd53b
PlatinPython <robert.alexander.kaempf@gmail.com>
2023-05-17 19:53:32 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bonk Bat
+
+## WIP
---
+++ src/main/java/platinpython/bonkbat/BonkBat.java
@@ -0,0 +1,15 @@
+package platinpython.bonkbat;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod(BonkBat.MOD_ID)
+public class BonkBat {
+    public static final String MOD_ID = "bonkbat";
+
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public BonkBat() {
+    }
+}
