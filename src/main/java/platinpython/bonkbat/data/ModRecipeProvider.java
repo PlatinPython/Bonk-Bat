a7b9bda458179320823acf7a4cc93be62a63a2e8
PlatinPython <robert.alexander.kaempf@gmail.com>
2024-08-09 21:26:31 +0200

    Group bat recipes.

--- src/main/java/platinpython/bonkbat/data/ModRecipeProvider.java
+++ src/main/java/platinpython/bonkbat/data/ModRecipeProvider.java
@@ -5,7 +5,9 @@ import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.RecipeProvider;
 import net.minecraft.data.recipes.ShapedRecipeBuilder;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Items;
+import platinpython.bonkbat.BonkBat;
 import platinpython.bonkbat.util.BatTypes;
 
 import java.util.function.Consumer;
@@ -19,6 +21,7 @@ public class ModRecipeProvider extends RecipeProvider {
     protected void buildRecipes(Consumer<FinishedRecipe> writer) {
         BatTypes.TYPES.forEach(
             (name, type) -> ShapedRecipeBuilder.shaped(RecipeCategory.COMBAT, type.bat().get())
+                .group(new ResourceLocation(BonkBat.MOD_ID, "bat").toString())
                 .define('W', type.wood().get())
                 .define('S', Items.STICK)
                 .pattern(" W ")
