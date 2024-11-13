5f0dc6d9e0595519e0de0afcf24a6a3e7ed30753
PlatinPython <robert.alexander.kaempf@gmail.com>
2024-11-13 16:11:16 +0100

    Disallow breaking blocks in creative while holding a bat. Fixes #1.

--- src/main/java/platinpython/bonkbat/item/BatItem.java
+++ src/main/java/platinpython/bonkbat/item/BatItem.java
@@ -1,8 +1,12 @@
 package platinpython.bonkbat.item;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Equipable;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
 
 public class BatItem extends Item implements Equipable {
     public BatItem() {
@@ -12,5 +16,10 @@ public class BatItem extends Item implements Equipable {
     @Override
     public EquipmentSlot getEquipmentSlot() {
         return EquipmentSlot.HEAD;
+    }
+
+    @Override
+    public boolean canAttackBlock(BlockState state, Level level, BlockPos pos, Player player) {
+        return !player.isCreative();
     }
 }
